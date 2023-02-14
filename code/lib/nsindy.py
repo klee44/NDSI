3cf6fde389c4ea4a06b16aeb5961dae212e010bb
Kookjin Lee <klee263@EN4202487-CIDSE.local>
2023-02-14 09:10:52 -0700

    adding ISTA example

--- code/lib/nsindy.py
+++ code/lib/nsindy.py
@@ -78,6 +78,47 @@ def train_prune(odefunc, lr, nepoch, niterbatch, lMB, nMB, odeint_method, train_
 	print(odefunc.C.weight)
 	torch.save({'state_dict': odefunc.state_dict(),}, ckpt_path)
 
+def proximal(w, lam=0.1, eta=0.1):
+	"""Proximal step"""
+	# w shape dim \times number of dicts
+	#dim, ndicts = w.shape
+	#tmp = w.flatten()	
+	alpha = torch.clamp(torch.abs(w) - lam * eta, min=0)
+	w.data = torch.sign(w) * alpha#).reshape((dim, ndicts))
+	'''
+	wadj = w.view(func.dims[0], -1, func.dims[0])  # [j, m1, i]
+	tmp = torch.sum(wadj**2, dim=1).pow(0.5) - lam * eta
+	alpha = torch.clamp(tmp, min=0)
+	v = torch.nn.functional.normalize(wadj, dim=1) * alpha[:, None, :]
+	w.data = v.view(-1, func.dims[0])
+	'''
+
+def train_ISTA(odefunc, lr, nepoch, niterbatch, lMB, nMB, odeint_method, train_data, t, l1_reg, ckpt_path, fig_save_path):
+	train_data = torch.tensor(train_data)
+	
+	params = odefunc.parameters()
+	optimizer = optim.Adamax(params, lr=lr)
+	scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.9987)
+	
+	best_loss = 1e30
+	frame = 0 
+	
+	for itr in range(nepoch):
+		#print('=={0:d}=='.format(itr))
+		for i in range(niterbatch):
+			optimizer.zero_grad()
+			batch_y0, batch_t, batch_y = utils.get_batch(train_data,t,lMB,nMB)
+			pred_y = odeint(odefunc, batch_y0, batch_t, method=odeint_method).transpose(0,1)
+			loss = torch.mean(torch.abs(pred_y - batch_y))
+			#l1_norm = 1e-4*torch.norm(odefunc.C.weight, p=1)
+			#loss += l1_norm
+			loss.backward(retain_graph = True)
+			optimizer.step()
+			proximal(odefunc.C.weight, lam=l1_reg, eta=0.01)
+		scheduler.step()
+		print(itr, loss.item())	
+	print(odefunc.C.weight)
+
 def test(odefunc, odeint_method, test_data, t, ckpt_path, fig_save_path):
 	odefunc.NFE = 0
 	test_sol = np.zeros_like(test_data)
