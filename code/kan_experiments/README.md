c099e5a97b8a0cd29bbbb965f505ee4f581d77ba
Kookjin Lee <klee263@en4202487-cidse.dhcp.asu.edu>
2024-05-29 11:33:54 -0700

    update

--- code/kan_experiments/README.md
+++ code/kan_experiments/README.md
@@ -1,14 +1,22 @@
 ## Overview
 Tested KANs as a parameterization technique for neural ODEs
 
+All implemented in PyTorch
+
+Required packages:
+- [torchdiffeq](https://github.com/rtqichen/torchdiffeq)
+
+- [efficient kan](https://github.com/Blealtan/efficient-kan) (included in the repo)
+
+
 ### to run the experiments on ODEBench (from ODEFormer)
 
 ```python
 python train_kan.py --kanlayer 3 2 --grid 5 --k 3 
 ```
 
-- use kanlayer to specify the kan architecture, 
+- use `kanlayer` to specify the kan architecture, 
 
-- use grid to specify the grid on which B-splines are defined 
+- use `grid` to specify the grid on which B-splines are defined 
 
-- use k to specify the degree of B-splines
+- use `k` to specify the degree of B-splines
--- code/kan_experiments/train_kan.py
+++ code/kan_experiments/train_kan.py
@@ -16,8 +16,6 @@ import matplotlib.pyplot as plt
 import lib.utils as utils
 from lib.odefunc import ODEfunc_KAN
 from torchdiffeq import odeint as odeint
-#from lib.torchdiffeq import odeint_adjoint as odeint
-#import lib.odeint as odeint
 
 import json
 
@@ -66,15 +64,6 @@ f.close()
 print(len(data))
 for i in range(63):
 	print(i,data[i]['eq_description'], len(data[i]['solutions'][0]))
-#print(data[i])
-'''
-print(len(data[26]['solutions'][0][0]['t']))
-print(len(data[26]['solutions'][0][0]['y'][0]))
-print(len(data[26]['solutions'][0][0]['y'][1]))
-print(len(data[26]['solutions'][0][1]['t']))
-print(len(data[26]['solutions'][0][1]['y'][0]))
-print(len(data[26]['solutions'][0][1]['y'][1]))
-'''
 
 for i in range(63):
 	id_path = os.path.join(save_path, "id_"+str(i)) 
@@ -167,90 +156,4 @@ for i in range(63):
 		
 		save_file = os.path.join(fig_save_path,"image_best.png")
 		plt.savefig(save_file)
-exit()
-'''
-		experimentID = str(i) 
-		ckpt_path = os.path.join(save_path, "experiment_" + str(experimentID) + '.ckpt')
-		fig_save_path = os.path.join(save_path,"experiment_"+str(experimentID))
-		utils.makedirs(fig_save_path)
-'''
-print(ckpt_path)
-t = np.array(data[26]['solutions'][0][0]['t'])
-print(t.shape)
 
-x = np.expand_dims(np.array(data[26]['solutions'][0][0]['y'][0]),-1)
-y = np.expand_dims(np.array(data[26]['solutions'][0][0]['y'][1]),-1)
-xy = np.concatenate((x,y), 1)
-
-t = torch.tensor(t).squeeze()
-
-train_data = torch.tensor(xy).unsqueeze(0)
-
-print(train_data.shape)
-
-odefunc = ODEfunc(2, args.nlayer, args.nunit)
-
-params = odefunc.parameters()
-optimizer = optim.Adamax(params, lr=args.lr)
-scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.9987)
-
-best_loss = 1e30
-frame = 0 
-
-for itr in range(args.nepoch):
-	print('=={0:d}=='.format(itr))
-	#for mb_data in train_data:
-	for i in range(args.niterbatch):
-		optimizer.zero_grad()
-		batch_y0, batch_t, batch_y, _, _, _ = utils.get_batch_two_single(train_data,t,args.lMB,args.nMB)
-		pred_y = odeint(odefunc, batch_y0, batch_t, method=args.odeint).to(device).transpose(0,1)
-		loss = torch.mean(torch.abs(pred_y - batch_y))
-		print(itr,i,loss.item())
-		loss.backward()
-		optimizer.step()
-	scheduler.step()
-
-	with torch.no_grad():
-		pred_y = odeint(odefunc, train_data[:,0,:], t, method=args.odeint).to(device).transpose(0,1)
-		val_loss = torch.mean(torch.abs(pred_y - train_data)).item()
-		print('val loss', val_loss)
-			
-		if best_loss > val_loss:
-			print('saving...', val_loss)
-			torch.save({'state_dict': odefunc.state_dict(),}, ckpt_path)
-			best_loss = val_loss 
-
-		plt.figure()
-		plt.tight_layout()
-		save_file = os.path.join(fig_save_path,"image_{:03d}.png".format(frame))
-		fig = plt.figure(figsize=(8,4))
-		axes = []
-		for i in range(2):
-			axes.append(fig.add_subplot(1,2,i+1))
-			axes[i].plot(t,train_data[0,:,i].detach().numpy(),lw=2,color='k')
-			axes[i].plot(t,pred_y.detach().numpy()[0,:,i],lw=2,color='c',ls='--')
-			plt.savefig(save_file)
-		plt.close(fig)
-		plt.close('all')
-		plt.clf()
-		frame += 1
-
-ckpt = torch.load(ckpt_path)
-odefunc.load_state_dict(ckpt['state_dict'])
-
-odefunc.NFE = 0
-test_loss = 0
-
-pred_y = odeint(odefunc, train_data[:,0,:], t, method=args.odeint).to(device).transpose(0,1)
-test_loss = torch.mean(torch.abs(pred_y - train_data)).item()
-print('test loss', test_loss)
-
-fig = plt.figure(figsize=(8,4))
-axes = []
-for i in range(2):
-	axes.append(fig.add_subplot(1,2,i+1))
-	axes[i].plot(t,train_data[0,:,i],lw=3,color='k')
-	axes[i].plot(t,pred_y.detach().numpy()[0,:,i],lw=2,color='c',ls='--')
-
-save_file = os.path.join(fig_save_path,"image_best.png")
-plt.savefig(save_file)
